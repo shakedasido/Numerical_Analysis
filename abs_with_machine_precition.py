6465683c0377719f345c088a967e510f2a8fe8e2
koral <kor123123@gmail.com>
2022-03-09 14:18:45 +0200

    check if equal

--- abs_with_machine_precition.py
+++ abs_with_machine_precition.py
@@ -4,3 +4,7 @@ flag = True
 while flag:
     final_epsilon = epsilon
     epsilon = epsilon / 2.0
+    if 1.0 + epsilon == 1.0:
+        flag = False
+
+print(abs(3.0 * (4.0 / 3.0 - 1) - 1) - final_epsilon)
